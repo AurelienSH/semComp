29ca087443d1a1e3478b01b6e918b5b731d7b78e
aurel <aurelien.said-housseini@sorbonne-nouvelle.fr>
2023-01-22 13:01:11 +0100

    Commentaires bis

--- word2vec.py
+++ word2vec.py
@@ -1,20 +1,31 @@
 """
 Script de vectorisation avec Word2Vec utilisant gensim
 """
+
+# ============================================================= #
+
+# vectorisation
 from gensim.models import Word2Vec, KeyedVectors
+
+# sauvegarde
 import pickle
+
+# récupération des arguments en ligne de commande
 import sys
 
+# ============================================================= #
+
 if __name__ == "__main__":
-    # Model parameters
-    model=Word2Vec(window=5, min_count=2, workers=4, sg=0) # Hyperparamètres du model
-    with open("./corpus/pickledTokenizedCorpus", "r+b") as f: # Chargement du corpus
+    
+    # paramétrage du modèle word2vec
+    model = Word2Vec(window=5, min_count=2, workers=4, sg=0)
+    
+    # Chargement du corpus depuis le fichier pickled
+    with open("./corpus/pickledTokenizedCorpus", "r+b") as f: 
         corpus = pickle.load(f)
 
-
     size = int(sys.argv[1]) # Taille d'échantillonnage en nombre de phrases
-
-    corpus = corpus[:size]
+    corpus = corpus[:size] # échantillonnage du corpus
 
     # Entrainement du modèle
     model.build_vocab(corpus, progress_per=3000)
